9493a6fb9bdaa154d481c63973d74d117cca4a13
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-03-02 20:30:53 +0300

    Error message added

--- src/js/appCreator.js
+++ src/js/appCreator.js
@@ -2,17 +2,25 @@ export default class AppCreator {
   constructor() {
     this.html = `
       <form class='container'>
-        <input type='text' placeholder='login' required>
         <button type='submit' class='container__button'>Click me if you want to popover</button>
+        <input type='text' class='container__input' placeholder='login' required>
+        <div class='popup hidden'></div>
       </form>
     `;
+    this.errorMessage = {
+      valueMissing: 'Тут пусто',
+    };
   }
 
   init() {
     document.body.innerHTML = this.html;
     this.button = document.querySelector('.container__button');
-    this.button.addEventListener('click', (e) => {
-      // e.preventDefault();
+    this.form = document.querySelector('form');
+    this.input = document.querySelector('input');
+    this.button.addEventListener('click', () => {
+      const errorInput = [...this.form.elements].find((el) => !el.validity.valid);
+      console.log(this.form.elements);
+      errorInput.setCustomValidity(this.errorMessage.valueMissing);
       this.button.classList.toggle('active');
     });
   }
